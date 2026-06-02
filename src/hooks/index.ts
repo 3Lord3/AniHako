cce540352643f7a4a20e84c0d75cc9af04609964
Lorot <troller333@mail.ru>
2026-06-02 17:03:12 +0300

    chore: remove unused FavoritesPage, components and hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from './useDebounce';
 export * from './useAuth';
 export * from './useAnime';
-export * from './useSuspenseQuery';
 export * from './useTheme';
 export * from './useTournament';
